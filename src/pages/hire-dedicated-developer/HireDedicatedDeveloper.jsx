2065ec8d78692afb4905f7683aaeb496d00c53bd
Vinod Pawar <vrpawar4567@gmail.com>
2022-09-18 01:01:17 +0530

    Vercel Update

--- src/pages/hire-dedicated-developer/HireDedicatedDeveloper.jsx
+++ src/pages/hire-dedicated-developer/HireDedicatedDeveloper.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Base from "../../core/Base";
-import { HireReactData } from "../../data/hireDedicatedDevelopers";
-import TopBanner from "../../component/HireDedicated/TopBanner/TopBanner";
-import Expertise from "../../component/HireDedicated/Expertise/Expertise";
-import WhyTech from "../../component/HireDedicated/WhyTech/WhyTech";
+// import { HireReactData } from "../../data/hireDedicatedDevelopers";
+// import TopBanner from "../../component/HireDedicated/TopBanner/TopBanner";
+// import Expertise from "../../component/HireDedicated/Expertise/Expertise";
+// import WhyTech from "../../component/HireDedicated/WhyTech/WhyTech";
 const HireDedicatedDeveloper = () => {
   return (
     <Base>
--- src/pages/hire-dedicated-developer/hire-angular-developers - Copy.jsx
+++ src/pages/hire-dedicated-developer/hire-angular-developers - Copy.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { HireAngularData } from "../../data/hireDedicated/angular";
-import { HireReviewData } from "../../data/hireDedicated/review";
-import { HireCaseStudiesData } from "../../data/hireDedicated/caseStudies";
-import HeadMeta from "../../component/Navigation/HeadMeta";
-import TopBanner from "../../component/HireDedicated/TopBanner/TopBanner";
-import Expertise from "../../component/HireDedicated/Expertise/Expertise";
-import WhyTech from "../../component/HireDedicated/WhyTech/WhyTech";
-import ReviewSlider from "../../component/HireDedicated/ReviewSlider/ReviewSlider";
-import CaseStudies from "../../component/CaseStudies/CaseStudies";
-import Faq from "../../component/HireDedicated/Faq/Faq";
-import Contact from "../../component/HireDedicated/Contact/Contact";
-const HireDedicatedAngular = () => {
-  return (
-    <>
-      <HeadMeta allMeta={HireAngularData} />
-      <TopBanner allData={HireAngularData} />
-      <Expertise expertiseData={HireAngularData} />
-      <WhyTech whyTechData={HireAngularData} />
-      <ReviewSlider ReviewSliderData={HireReviewData} />
-      <CaseStudies CaseStudyData={HireCaseStudiesData} />
-      <Faq faqsData={HireAngularData} />
-      <Contact />
-    </>
-  );
-};
+// import React from "react";
+// import { HireAngularData } from "../../data/hireDedicated/angular";
+// import { HireReviewData } from "../../data/hireDedicated/review";
+// import { HireCaseStudiesData } from "../../data/hireDedicated/caseStudies";
+// import HeadMeta from "../../component/Navigation/HeadMeta";
+// import TopBanner from "../../component/HireDedicated/TopBanner/TopBanner";
+// import Expertise from "../../component/HireDedicated/Expertise/Expertise";
+// import WhyTech from "../../component/HireDedicated/WhyTech/WhyTech";
+// import ReviewSlider from "../../component/HireDedicated/ReviewSlider/ReviewSlider";
+// import CaseStudies from "../../component/CaseStudies/CaseStudies";
+// import Faq from "../../component/HireDedicated/Faq/Faq";
+// import Contact from "../../component/HireDedicated/Contact/Contact";
+// const HireDedicatedAngular = () => {
+//   return (
+//     <>
+//       <HeadMeta allMeta={HireAngularData} />
+//       <TopBanner allData={HireAngularData} />
+//       <Expertise expertiseData={HireAngularData} />
+//       <WhyTech whyTechData={HireAngularData} />
+//       <ReviewSlider ReviewSliderData={HireReviewData} />
+//       <CaseStudies CaseStudyData={HireCaseStudiesData} />
+//       <Faq faqsData={HireAngularData} />
+//       <Contact />
+//     </>
+//   );
+// };
 
-export default HireDedicatedAngular;
+// export default HireDedicatedAngular;
