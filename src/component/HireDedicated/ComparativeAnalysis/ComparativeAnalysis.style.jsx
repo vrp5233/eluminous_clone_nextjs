c987a41df5cf5ccd4177c4f8ee347bc16a59e227
vrp5233 <eluminous.design11@gmail.com>
2022-10-03 19:19:39 +0530

    Table add

--- src/component/HireDedicated/ComparativeAnalysis/ComparativeAnalysis.style.jsx
+++ src/component/HireDedicated/ComparativeAnalysis/ComparativeAnalysis.style.jsx
@@ -1,5 +1,49 @@
 import styled from "styled-components";
 
 export const ComparativeAnalysisWrapper = styled.section`
- background-color: red;
+  .title {
+    margin-bottom: 50px;
+  }
+  .custom_table {
+    max-width: 1150px;
+    margin: 0 auto 70px;
+    thead th,
+    tbody td {
+      padding: 0.75rem;
+      &:first-child {
+        text-align: left;
+      }
+    }
+    thead th {
+      background-color: rgba(var(--blackTurmeric));
+      color: #fff;
+      font-size: 18px;
+      font-weight: 600;
+      border: 1px solid rgba(var(--blackTurmeric));
+      vertical-align: middle;
+      text-align: center;
+      &:nth-child(2) {
+        background-color: #2166f3;
+        border: 1px solid #2166f3;
+      }
+    }
+    tbody th,
+    tbody td {
+      border: 1px solid #dee2e6;
+    }
+    tbody th {
+      font-weight: 500;
+      background-color: rgb(240, 240, 240);
+    }
+    tbody td {
+      text-align: center;
+      &:nth-child(2) {
+        color: #048d00;
+        font-weight: 600;
+        span {
+          color: rgba(var(--blackTurmeric));
+        }
+      }
+    }
+  }
 `;
---
+++ src/data/hireDedicated/ComparativeAnalysisData.js
@@ -0,0 +1,117 @@
+export const ComparativeAnalysisData = {
+  theads: [
+    {
+      parameter: "Time to get right developers",
+      eLuminous: "eLuminous",
+      freelance: "Freelance Developers",
+      inhouse: "In-house Developers",
+    },
+  ],
+  tbodies: [
+    {
+      parameter: "Time to get right developers",
+      eLuminous: "1 day - 2 weeks",
+      freelance: "4 - 12 weeks",
+      inhouse: "1 - 12 weeks",
+    },
+    {
+      parameter: "Time to start a project",
+      eLuminous: "1 day - 2 weeks	",
+      freelance: "1 - 12 weeks",
+      inhouse: "2 - 10 weeks",
+    },
+    {
+      parameter: "Acquisition cost",
+      eLuminous: "0",
+      freelance: "0",
+      inhouse: "$9,000 - $25,000",
+    },
+    {
+      parameter: "Recurring cost of training & benefits",
+      eLuminous: "0",
+      freelance: "0",
+      inhouse: "$5,000 - $10,000",
+    },
+    {
+      parameter: "Pricing (weekly Average)",
+      eLuminous: "1.5x",
+      freelance: "1x",
+      inhouse: "4x",
+    },
+    {
+      parameter: "Time to scale size of team",
+      eLuminous: "1 day - 2 weeks",
+      freelance: "1 - 12 weeks",
+      inhouse: "4 - 16 weeks",
+    },
+    {
+      parameter: "Project failure risk",
+      eLuminous:
+        "Extremely Low <br /> <span> We have <b>97% </b>Success Ratio</span>",
+      freelance: "Very High",
+      inhouse: "Low",
+    },
+    {
+      parameter: "Termination costs",
+      eLuminous: "NONE",
+      freelance: "None",
+      inhouse: "High",
+    },
+    {
+      parameter: "Developers backed by a delivery team",
+      eLuminous: "YES",
+      freelance: "No",
+      inhouse: "Some",
+    },
+    {
+      parameter: "Dedicated resources",
+      eLuminous: "YES",
+      freelance: "No",
+      inhouse: "Yes",
+    },
+    {
+      parameter: "Agile Development methodology",
+      eLuminous: "YES",
+      freelance: "No",
+      inhouse: "Some",
+    },
+    {
+      parameter: "Quality assuarance",
+      eLuminous: "YES",
+      freelance: "Some",
+      inhouse: "No",
+    },
+    {
+      parameter: "Impact due to turnover",
+      eLuminous: "NONE",
+      freelance: "High",
+      inhouse: "High",
+    },
+    {
+      parameter: "Structured training programs",
+      eLuminous: "YES",
+      freelance: "No",
+      inhouse: "Some",
+    },
+    {
+      parameter: "Communications",
+      eLuminous: "Seamless",
+      freelance: "Uncertain",
+      inhouse: "Seamless",
+    },
+    {
+      parameter: "Assured work rigor",
+      eLuminous: "40 hrs/week",
+      freelance: "Uncertain",
+      inhouse: "40 hrs/week",
+    },
+    {
+      parameter: "Tools and professional environment",
+      eLuminous: "Best-in-Class",
+      freelance: "No",
+      inhouse: "Varies",
+    },
+  ],
+  btnLink: "#hireDedicatedForm",
+  btnText: "Get Started Today",
+};
